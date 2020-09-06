871919a160b380a45dc8207cd819c8981dde08dc
Nikolay Masson <nick.masson@yandex.ru>
2020-09-07 01:49:42 +0300

    Создал скелет контроллера и интерфейс для репозитория

---
+++ GenericMockApi/Controllers/RandomMockValuesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GenericMockApi.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GenericMockApi.Controllers
+{
+    [Route("/api/mockdata")]
+    [ApiController]
+    public class RandomMockValuesController : ControllerBase
+    {
+        private readonly IMockDataRepository _repository;
+
+        public RandomMockValuesController(IMockDataRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet("{typeName}")]
+        public ActionResult GetMockData(string typeName, int skip, int take)
+        {
+            var data = _repository.GetMockObjects(typeName, skip, take);
+            if (data == null) return BadRequest();
+            else return Ok(data);
+        }
+    }
+}
---
+++ GenericMockApi/Options/MockGeneratorOptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericMockApi.Options
+{
+    public class MockGeneratorOptions
+    {
+        public int DepthLimit { get; set; }
+        public int MasterSeed { get; set; }
+    }
+}
