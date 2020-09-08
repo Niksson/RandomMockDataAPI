0f89876303848d5382cc3bdeb83a89ec29d69996
Nikolay Masson <nick.masson@yandex.ru>
2020-09-08 15:49:11 +0300

    Добавил метод-помощник для определения, является ли тип числовым

---
+++ GenericMockApi/Helpers/TypeHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GenericMockApi.Helpers
+{
+    public class TypeHelpers
+    {
+        public static bool IsTypeNumericOrChar(Type type)
+        {
+            var numericTypes = new List<Type>()
+            {
+                typeof(int),
+                typeof(int),
+                typeof(double),
+                typeof(decimal),
+                typeof(float),
+                typeof(byte),
+                typeof(sbyte),
+                typeof(short),
+                typeof(long),
+                typeof(uint),
+                typeof(ulong),
+                typeof(ushort),
+                // char can be cast from any numeric type (integral or floating point
+                // so we can generate chars via numeric generator
+                typeof(char)
+            };
+
+            return numericTypes.Contains(type);
+        }
+    }
+}
